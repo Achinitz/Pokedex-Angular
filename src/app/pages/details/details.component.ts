c54f92f31743b3335b3fdd22577af8c29ee8bfcb
Gustavo de Oliveira Achinitz <F0436@fcopel.org.br>
2022-05-30 08:29:52 -0300

    Aplicacao Completa

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,43 @@
+import { forkJoin } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit {
+
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(private activatedRoute: ActivatedRoute, private pokeApiService: PokeApiService) { }
+
+  ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  public getPokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon,name]).subscribe(
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+        this.apiError = false;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
+
+  }
+
+}
